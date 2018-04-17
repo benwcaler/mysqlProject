87e80a3a987e9f8b5d5e0b2f81bb143c1153e782
Ben <b.wayne@me.com>
2018-04-17 13:53:28 -0600

    table and messages

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table2");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -12,11 +13,64 @@ connection.connect(function(err) {
   if (err) throw err;
   itemDisplay();
   // whatToDo();
-})
+});
 
 function itemDisplay() {
   //display all the items in the database
+  var table = new Table({
+    head: ["Item ID", "Name", "Department", "Price", "Stock"],
+    chars: {
+      top: "═",
+      "top-mid": "╤",
+      "top-left": "╔",
+      "top-right": "╗",
+      bottom: "═",
+      "bottom-mid": "╧",
+      "bottom-left": "╚",
+      "bottom-right": "╝",
+      left: "║",
+      "left-mid": "╟",
+      mid: "─",
+      "mid-mid": "┼",
+      right: "║",
+      "right-mid": "╢",
+      middle: "│"
+    },
+    colWidths: [9, 20, 16, 13, 7]
+  });
   connection.query("SELECT * FROM products", function(err, res) {
-    console.log(res);
+    // console.log(res);
+    for (var i = 0; i < res.length; i++) {
+      var item = [];
+      item.push(
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        "$" + res[i].price,
+        res[i].stock_quantity
+      );
+      table.push(item);
+      console.log(table);
+    }
+    console.log(table.toString());
+    nowWhat();
+  });
+  connection.end();
+}
+
+function nowWhat() {
+  inquirer.prompt([
+    {
+      message: "Enter the item number of the item you would like to buy: ",
+      type: "input",
+      name: "number"
+    },
+    {
+      message: "How many?",
+      type: "input",
+      name: "quantity"
+    }
+  ]).then(function(response) {
+    
   })
 }
